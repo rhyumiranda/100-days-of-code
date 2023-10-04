682e33905026f3550027e11ed1366df98dfb89f5
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2023-10-04 23:37:30 +0800

    Create a function that would accept two values

--- Day 1-10/Day 1/TipCalculator/js/TipCalculator.js
+++ Day 1-10/Day 1/TipCalculator/js/TipCalculator.js
@@ -1,1 +1,36 @@
-console.log("Hello world, this is a test!")
+//Things to do:
+
+// 1. Create a function that would acccept the amount of bill
+// 2. Create ... that would accept tip percentage
+      // >> Convert integer form into decimal
+// 3. Print the amount of tip
+// 4. Print amount of bill + tip
+
+// Additional Features 
+// 1. People who will split the bill + tip
+// 2. Print tip per person
+
+function calculateTip(){
+  
+}
+
+function getAmountOfBill(){
+  let amount = parseFloat(prompt("Kindly enter the amount of your bill. Thank you!"));
+  return amount;
+}
+
+function getTipPercentage(){
+  let percentage;
+  
+  do{
+    percentage = parseFloat(prompt("Please enter the percentage of tip you want to give."));
+  } while (Number.isNaN(Number(percentage)));
+  
+  return percentage;
+}
+
+const amountOfBill = getAmountOfBill();
+const tipPercentage = getTipPercentage();
+
+console.log(typeof(amountOfBill));
+console.log(typeof(tipPercentage));
