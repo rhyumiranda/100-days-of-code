135259f1326d01de5cc06073684175b3a78ca5c6
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2023-10-10 00:42:39 +0800

    Rename array to become appropriate

--- Day 1-10/Day 3/bmiCalculator/js/bmiCalculator.js
+++ Day 1-10/Day 3/bmiCalculator/js/bmiCalculator.js
@@ -1,18 +1,31 @@
 function calculateBMI(){
-  let weightUnitChoice = ["lbs", "kg"];
-  let heightUnitChoice = ["in", "m"];
-
+  let unitOptions = ["standard", "metric"];
+  let weight;
   let height;
+  let bmi;
 
   alert("Welcome to BMI Calculator");
 
   do{
-    getWeight = prompt("Enter unit for weight lbs, kg")
-  } while (!weightUnitChoice.includes(getWeight));
+    unitChoice = prompt("Enter Standard | Enter Metric")
+    unitChoiceLower = unitChoice.toLowerCase();
+  } while (!unitOptions.includes(unitChoiceLower));
 
-  do{
-    height = parseFloat(prompt("Enter your height in inches"));
-  } while (Number.isNaN(Number(height)) || height <= 0);
+  if(unitChoice == "standard"){
+    do{
+      weight = parseFloat(prompt("Enter your weight in lbs"));
+    } while (Number.isNaN(Number(weight)) || weight <= 0);
+
+    do{
+      height = parseFloat(prompt("Enter your height in inches"));
+    } while (Number.isNaN(Number(height)) || height <= 0);
+
+  } else if (unitChoice == "metric"){
+    do{
+      height = parseFloat(prompt("Enter your height in meters"));
+    } while (Number.isNaN(Number(height)) || height <= 0);
+  }
+  
   
   
 }
