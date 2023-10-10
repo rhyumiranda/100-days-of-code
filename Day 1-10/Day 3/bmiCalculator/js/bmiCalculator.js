b5d2b4acdf2cee4fb79198f12495dfa87b5f9468
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2023-10-10 13:52:22 +0800

    Add a function that prevents case sensitive inputs

--- Day 1-10/Day 3/bmiCalculator/js/bmiCalculator.js
+++ Day 1-10/Day 3/bmiCalculator/js/bmiCalculator.js
@@ -1,15 +1,28 @@
-function calculateBMI(){
-  let unitOptions = ["standard", "metric"];
-  let weight;
-  let height;
-  let bmi;
+function getUnitChoice(){
+  const units = ["standard", "metric"]
+  const userInput = false;
 
   alert("Welcome to BMI Calculator");
 
-  do{
-    unitChoice = prompt("Enter Standard | Enter Metric")
-    unitChoiceLower = unitChoice.toLowerCase();
-  } while (!unitOptions.includes(unitChoiceLower));
+  while(userInput == false){
+    const choice = prompt("Standard or Metric?");
+    if (choice == null){
+      continue;
+    }
+
+    const choiceLower = choice.toLowerCase();
+    if(units.includes(choiceLower)){
+      userInput  == true;
+      return choiceLower;
+    }
+  }
+}
+
+function calculateBMI(){
+  const unitChoice = getUnitChoice();
+  let weight;
+  let height;
+  let bmi;
 
   if(unitChoice == "standard"){
     do{
@@ -20,13 +33,21 @@ function calculateBMI(){
       height = parseFloat(prompt("Enter your height in inches"));
     } while (Number.isNaN(Number(height)) || height <= 0);
 
+    height *= 2;
+
+
   } else if (unitChoice == "metric"){
+    do{
+      weight = parseFloat(prompt("Enter your weight in kg"));
+    } while (Number.isNaN(Number(weight)) || weight <= 0);
+
     do{
       height = parseFloat(prompt("Enter your height in meters"));
     } while (Number.isNaN(Number(height)) || height <= 0);
+
+    height *= height;
+    bmi = (weight / height);
   }
-  
-  
-  
 }
+
 calculateBMI();
